c2107ddf021681c6627314cd7bcc3edad07bd0cf
Innaky <innaky@protonmail.com>
2019-01-30 00:04:47 -0400

    when and unless macros.

--- a-some-macros.lisp
+++ a-some-macros.lisp
@@ -1,5 +1,13 @@
-(defmacro nle (n letargs &rest body)
+(defmacro nlet (n letargs &rest body)
   "A let of scheme."
   `(labels ((,n ,(mapcar #'car letargs)
 	      ,@body))
      (,n ,@(mapcar #'cadr letargs))))
+
+(defmacro when (test &rest body)
+  `(if ,test
+       (progn ,@body)))
+
+(defmacro unless (test &rest body)
+  `(if (not ,test)
+       (progn ,@body)))
