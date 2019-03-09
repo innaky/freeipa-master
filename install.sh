1e29d55f5db89f38eb9078dffab4abe24888e93b
Innaky <innaky@protonmail.com>
2019-03-09 02:38:53 -0400

    (fix) adding corrections for automatic installation.

--- install.sh
+++ install.sh
@@ -5,10 +5,13 @@ curl -O https://beta.quicklisp.org/quicklisp.lisp
 wget -c http://prdownloads.sourceforge.net/sbcl/sbcl-1.4.15-x86-64-linux-binary.tar.bz2
 tar xjvf sbcl-1.4.15-x86-64-linux-binary.tar.bz2
 cd sbcl-1.4.15-x86-64-linux
-cd
+./install.sh
+cd ..
 
 sbcl --load quicklisp.lisp \
      --eval '(quicklisp-quickstart:install)' \
      --eval '(ql:quickload "quicklisp-slime-helper")' \
      --eval '(ql:add-to-init-file)' \
      --eval '(quit)'
+
+sbcl --load freeipa-master.lisp
