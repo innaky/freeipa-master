37c26d24fc0384978acaac28eb56972aa708f3c2
Innaky <innaky@protonmail.com>
2019-05-09 12:51:18 -0400

    freeipa-master install Non standard but easy for call the freeipa-master:main function.

--- install.sh
+++ install.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-yum -y install curl wget bzip2
-curl -O https://beta.quicklisp.org/quicklisp.lisp
-wget -c http://prdownloads.sourceforge.net/sbcl/sbcl-1.4.15-x86-64-linux-binary.tar.bz2
-tar xjvf sbcl-1.4.15-x86-64-linux-binary.tar.bz2
-cd sbcl-1.4.15-x86-64-linux
-./install.sh
-cd ..
-
-sbcl --load quicklisp.lisp \
-     --eval '(quicklisp-quickstart:install)' \
-     --eval '(ql:quickload "quicklisp-slime-helper")' \
-     --eval '(ql:add-to-init-file)' \
+sbcl --eval '(ql:quickload "freeipa-master")' \
+     --eval '(freeipa-master:main)' \
      --eval '(quit)'
-
-sbcl --load freeipa-master.lisp
